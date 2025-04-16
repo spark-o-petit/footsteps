1d776f0c61398db224cc238bda36b6d58947534b
spark-o-petit <petitspark247@gmail.com>
2025-04-16 14:13:56 +0900

    Initial Commit

---
+++ footsteps/footstepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  footstepsApp.swift
+//  footsteps
+//
+//  Created by Alex on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct footstepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
