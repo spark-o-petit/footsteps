ab2f0d296a44071c0931ce910c35e822fb063480
spark-o-petit <petitspark247@gmail.com>
2025-04-18 17:17:41 +0900

    Import SwiftData on ContentView

--- footsteps/ContentView.swift
+++ footsteps/ContentView.swift
@@ -6,86 +6,95 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Query var goals: [Goal]
+    
     var body: some View {
-        NavigationStack {
-            
-            List {
-                Section {
-                    NavigationLink(destination: Text("Simple App with SwiftUI")) {
-                            VStack (alignment: .leading) {
-                            Text("Simple App with SwiftUI")
-                                HStack {
-                                    Label("12 d", systemImage: "calendar")
-                                    Text("|")
-                                    Label("Normal", systemImage: "gauge.with.needle.fill")
-                                    Text("|")
-                                    Label("5/40", systemImage: "figure.walk")
-                                }
-                                .font(.subheadline)
-                                .foregroundColor(.accentColor)
-                        }
-                    }
-                } header: {
-                    HStack {
-                        Image(systemName: "swift")
-                        Text("TECH")
-                    }
-                }
-                Section {
-                    NavigationLink(destination: Text("Simple App with SwiftUI")) {
-                            VStack (alignment: .leading) {
-                            Text("Simple App with SwiftUI")
-                                HStack {
-                                    Label("12 d", systemImage: "calendar")
-                                    Text("|")
-                                    Label("Normal", systemImage: "gauge.with.needle.fill")
-                                    Text("|")
-                                    Label("5/40", systemImage: "figure.walk")
-                                }
-                                .font(.subheadline)
-                                .foregroundColor(.accentColor)
-                        }
-                    }
-                } header: {
-                    HStack {
-                        Image(systemName: "pencil.and.outline")
-                        Text("DESIGN")
-                    }
-                }
-                Section {
-                    NavigationLink(destination: Text("Simple App with SwiftUI")) {
-                            VStack (alignment: .leading) {
-                            Text("Simple App with SwiftUI")
-                                HStack {
-                                    Label("12 d", systemImage: "calendar")
-                                    Text("|")
-                                    Label("Normal", systemImage: "gauge.with.needle.fill")
-                                    Text("|")
-                                    Label("5/40", systemImage: "figure.walk")
-                                }
-                                .font(.subheadline)
-                                .foregroundColor(.accentColor)
-                        }
-                    }
-                } header: {
-                    HStack {
-                        Image(systemName: "chart.pie.fill")
-                        Text("BUSINESS")
-                    }
-                }
-
-              .navigationTitle("FOOTSTEPS")
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {}) {
-                        Image(systemName: "plus")
-                    }
-                }
-            }
-          }
+        
+        List(goals) { goal in
+            Text(goal.title)
+        }
+        
+        
+//        NavigationStack {
+//            
+//            List {
+//                Section {
+//                    NavigationLink(destination: Text("Simple App with SwiftUI")) {
+//                            VStack (alignment: .leading) {
+//                            Text("Simple App with SwiftUI")
+//                                HStack {
+//                                    Label("12 d", systemImage: "calendar")
+//                                    Text("|")
+//                                    Label("Normal", systemImage: "gauge.with.needle.fill")
+//                                    Text("|")
+//                                    Label("5/40", systemImage: "figure.walk")
+//                                }
+//                                .font(.subheadline)
+//                                .foregroundColor(.accentColor)
+//                        }
+//                    }
+//                } header: {
+//                    HStack {
+//                        Image(systemName: "swift")
+//                        Text("TECH")
+//                    }
+//                }
+//                Section {
+//                    NavigationLink(destination: Text("Simple App with SwiftUI")) {
+//                            VStack (alignment: .leading) {
+//                            Text("Simple App with SwiftUI")
+//                                HStack {
+//                                    Label("12 d", systemImage: "calendar")
+//                                    Text("|")
+//                                    Label("Normal", systemImage: "gauge.with.needle.fill")
+//                                    Text("|")
+//                                    Label("5/40", systemImage: "figure.walk")
+//                                }
+//                                .font(.subheadline)
+//                                .foregroundColor(.accentColor)
+//                        }
+//                    }
+//                } header: {
+//                    HStack {
+//                        Image(systemName: "pencil.and.outline")
+//                        Text("DESIGN")
+//                    }
+//                }
+//                Section {
+//                    NavigationLink(destination: Text("Simple App with SwiftUI")) {
+//                            VStack (alignment: .leading) {
+//                            Text("Simple App with SwiftUI")
+//                                HStack {
+//                                    Label("12 d", systemImage: "calendar")
+//                                    Text("|")
+//                                    Label("Normal", systemImage: "gauge.with.needle.fill")
+//                                    Text("|")
+//                                    Label("5/40", systemImage: "figure.walk")
+//                                }
+//                                .font(.subheadline)
+//                                .foregroundColor(.accentColor)
+//                        }
+//                    }
+//                } header: {
+//                    HStack {
+//                        Image(systemName: "chart.pie.fill")
+//                        Text("BUSINESS")
+//                    }
+//                }
+//
+//              .navigationTitle("FOOTSTEPS")
+//            }
+//            .toolbar {
+//                ToolbarItem(placement: .navigationBarTrailing) {
+//                    Button(action: {}) {
+//                        Image(systemName: "plus")
+//                    }
+//                }
+//            }
+//          }
     }
 }
 
