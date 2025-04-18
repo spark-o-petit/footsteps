f4bd28ba8bb8f1c9caa1741b764ba5725c43110a
spark-o-petit <petitspark247@gmail.com>
2025-04-18 17:10:45 +0900

    Edit data structure with swiftdata

--- footsteps/Model/Goals.swift
+++ footsteps/Model/Goals.swift
@@ -6,28 +6,33 @@
 //
 
 import Foundation
+import SwiftData
 
-struct Goal: Identifiable {
-    let id = UUID()
-    let title: String
-    let estimatedEfforts: String
-    let category: Category
-    let difficulty: Difficulty
-    let dueDate: Date
+@Model
+class Goal {
+    var title: String
+    var estimatedEfforts: String
+    var category: Category
+    var difficulty: Difficulty
+    var dueDate: Date
+
+    init(title: String, estimatedEfforts: String, category: Category, difficulty: Difficulty, dueDate: Date) {
+        self.title = title
+        self.estimatedEfforts = estimatedEfforts
+        self.category = category
+        self.difficulty = difficulty
+        self.dueDate = dueDate
+    }
 }
 
-enum Category: String, CaseIterable, Identifiable {
+enum Category: String, CaseIterable, Codable {
     case tech = "Tech"
     case design = "Design"
     case business = "Business"
-    
-    var id: String { self.rawValue }
 }
 
-enum Difficulty: String, CaseIterable, Identifiable {
+enum Difficulty: String, CaseIterable, Codable {
     case easy = "Easy"
     case medium = "Medium"
     case hard = "Hard"
-    
-    var id: String { self.rawValue }
 }
